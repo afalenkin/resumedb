cfbfbfe55ab043bd21982748d0f0a769722a436e
Andrey Alenkin <oxqq@ya.ru>
2020-12-16 19:42:02 +0800

    Откорректирован delete()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,13 +8,13 @@ public class ArrayStorage {
     int resumeCount = 0; // количество резюме в хранилище
 
     void clear() {
-        Arrays.fill(storage, 0, resumeCount, null );
+        Arrays.fill(storage, 0, resumeCount, null);
         resumeCount = 0;
     }
 
     void save(Resume r) {
         for (int i = 0; i < resumeCount; i++) {
-            if ( storage[i].uuid.equals(r.uuid)) {
+            if (storage[i].uuid.equals(r.uuid)) {
                 System.out.println("База уже содержит резюме с таким ID");
                 return;
             }
@@ -25,26 +25,29 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         for (int i = 0; i < resumeCount; i++) {
-                if (storage[i].uuid.equals(uuid)) {
-                    return storage[i];
-                }
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
+        }
         System.out.println("Резюме с таким ID отсутствует в базе");
         return null;
     }
 
     void delete(String uuid) {
         boolean deleted = false;
-            for (int i = 0; i < resumeCount; i++) {
-                if (storage[i].uuid.equals(uuid)) { // удаляем резюме с нужным uuid
-                    storage[i] = null;
-                    deleted = true;
-                    resumeCount--;
-                }
-                if (deleted) { //если удалили элемент, то сдвигаем оставшиеся элементы в массиве влево
-                    storage[i] = storage[i + 1];
-                }
+        int emptyElement = 0;
+        for (int i = 0; i < resumeCount; i++) {
+            if (storage[i].uuid.equals(uuid)) { // удаляем резюме с нужным uuid
+                storage[i] = null;
+                deleted = true;
+                emptyElement = i;
+                resumeCount--;
+                break;
             }
+        }
+        if (deleted) { //если удалили элемент, то сдвигаем оставшиеся элементы в массиве влево
+            System.arraycopy(storage, (emptyElement + 1), storage, emptyElement, (resumeCount - emptyElement));
+        }
 
     }
 
@@ -52,9 +55,9 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-            Resume[] result = new Resume[resumeCount];
-            System.arraycopy(storage, 0, result, 0, resumeCount);
-            return result;
+        Resume[] result = new Resume[resumeCount];
+        System.arraycopy(storage, 0, result, 0, resumeCount);
+        return result;
 
     }
 
