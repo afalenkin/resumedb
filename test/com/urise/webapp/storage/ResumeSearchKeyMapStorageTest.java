698b6f2b1125681d9bdc91288d38dab5f29c6ada
Andrey Alenkin <oxqq@ya.ru>
2021-02-18 20:21:34 +0800

    HW9; return back AbstractMapStorageTest

--- test/com/urise/webapp/storage/ResumeSearchKeyMapStorageTest.java
+++ test/com/urise/webapp/storage/ResumeSearchKeyMapStorageTest.java
@@ -1,6 +1,6 @@
 package com.urise.webapp.storage;
 
-public class ResumeSearchKeyMapStorageTest extends AbstractStorageTest {
+public class ResumeSearchKeyMapStorageTest extends AbstractMapStorageTest {
 
     public ResumeSearchKeyMapStorageTest() {
         super(new ResumeSearchKeyMapStorage());
