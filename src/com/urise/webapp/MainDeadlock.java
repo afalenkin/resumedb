a17a613b8e9db47863862ff41ec0d18824146985
Andrey Alenkin <oxqq@ya.ru>
2021-02-26 19:31:46 +0800

    HW11; MainDeadLock fix

--- src/com/urise/webapp/MainDeadlock.java
+++ src/com/urise/webapp/MainDeadlock.java
@@ -1,77 +1,43 @@
 package com.urise.webapp;
 
 public class MainDeadlock {
-    public Integer firstInt = 10;
-    public Integer secondInt = 20;
 
     // method with deadlock
-    public int lockMethod(Integer first, Integer second) {
+    public static int lockMethod(Integer first, Integer second) {
         synchronized (first) {
-            System.out.println(Thread.currentThread().getName() + " захватил объект со значением " + first);
+            System.out.println(Thread.currentThread().getName() + " captured an object with a value " + first);
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(Thread.currentThread().getName() + " пытается захватить объект со значением " + second);
+            System.out.println(Thread.currentThread().getName() + " tries to grab an object with a value " + second);
             synchronized (second) {
-                System.out.println(Thread.currentThread().getName() + " захватил объект со значением " + second);
+                System.out.println(Thread.currentThread().getName() + " captured an object with a value " + second);
                 return first + second;
             }
         }
     }
 
-    // same method avoiding deadlock
-    public int nonLockMethod(Integer first, Integer second) {
-        synchronized (first > second ? first : second) {
-            System.out.println(Thread.currentThread().getName() + " захватил объект со значением " + first);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() + " пытается захватить объект со значением " + second);
-            synchronized (second) {
-                System.out.println(Thread.currentThread().getName() + " захватил объект со значением " + second);
-                return first + second;
-            }
-        }
+    public static void done() {
+        System.out.println("Thread have finished work");
     }
 
-    public void done() {
-        System.out.println("All threads have finished their work");
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        MainDeadlock nonDeadLock = new MainDeadlock();
-
-        Thread firstWithoutDeadlock = new Thread(() -> {
-            System.out.println(nonDeadLock.nonLockMethod(nonDeadLock.firstInt, nonDeadLock.secondInt));
-            nonDeadLock.done();
-        }, "First thread");
-
-        Thread secondWithoutDeadlock = new Thread(() -> {
-            System.out.println(nonDeadLock.nonLockMethod(nonDeadLock.secondInt, nonDeadLock.firstInt));
-            nonDeadLock.done();
-        }, "Second thread");
-
-        firstWithoutDeadlock.start();
-        secondWithoutDeadlock.start();
-
-        Thread.sleep(1000);
+    public static void main(String[] args) {
 
         System.out.println("\n DEADLOCK:");
 
-        MainDeadlock deadlock = new MainDeadlock();
+        Integer firstInt = 10;
+        Integer secondInt = 20;
 
         Thread first = new Thread(() -> {
-            deadlock.lockMethod(deadlock.firstInt, deadlock.secondInt);
-            deadlock.done();
+            lockMethod(firstInt, secondInt);
+            done();
         }, "First thread");
 
         Thread second = new Thread(() -> {
-            deadlock.lockMethod(deadlock.secondInt, deadlock.firstInt);
-            deadlock.done();
+            lockMethod(secondInt, firstInt);
+            done();
         }, "Second thread");
 
         first.start();
