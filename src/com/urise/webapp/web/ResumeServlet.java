a6e09038d4366d7e66d6671aec75cc80b336a959
Andrey Alenkin <oxqq@ya.ru>
2021-03-13 18:09:39 +0800

    HW15 include "web" package

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -4,6 +4,7 @@ import com.urise.webapp.Config;
 import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.Storage;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,13 @@ import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
 
-    private Storage storage = Config.get().getDbStorage();
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getDbStorage();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -43,10 +50,40 @@ public class ResumeServlet extends HttpServlet {
 
     private void printResumeTable(PrintWriter writer) {
         List<Resume> resumes = storage.getAllSorted();
-        writer.println("<table style=\"width:100%\">");
-        writer.println("<tr>\n<th>UUID</th>\n<th>Full name</th>\n </tr>\n");
+        writer.println("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "<style>\n" +
+                "table {\n" +
+                "  font-family: arial, sans-serif;\n" +
+                "  border-collapse: collapse;\n" +
+                "  width: 100%;\n" +
+                "}\n" +
+                "\n" +
+                "td, th {\n" +
+                "  border: 1px solid #dddddd;\n" +
+                "  text-align: left;\n" +
+                "  padding: 8px;\n" +
+                "}\n" +
+                "\n" +
+                "tr:nth-child(even) {\n" +
+                "  background-color: #dddddd;\n" +
+                "}\n" +
+                "</style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "\n" +
+                "<h2>All resumes in database</h2>\n" +
+                "\n" +
+                "<table>\n" +
+                "  <tr>\n");
+        writer.println("<tr>\n<th>" + "Resume UUID </th>\n<th> Resumes owner fullname </th>\n</tr>\n");
         resumes.forEach(resume -> writer.println("<tr>\n<th>" + resume.getUuid() + "</th>\n<th>" + resume.getFullName() +
                 "</th>\n</tr>\n"));
-        writer.println("</table>");
+        writer.println(
+                "</table>\n" +
+                        "\n" +
+                        "</body>\n" +
+                        "</html>");
     }
 }
